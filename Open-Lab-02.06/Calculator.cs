0942ad32991c87beef78ab7a0d4fe9aeefaa3bec
Erik Zemčík <zemcik.erik@gmail.com>
2019-10-24 17:52:02 +0200

    Created test

---
+++ Open-Lab-02.06/Calculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Open_Lab_02._06
+{
+    public class Calculator
+    {
+        public bool Divisible(int number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Open-Lab-02.06/Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace Open_Lab_02._06
+{
+    public class Tests
+    {
+
+        private Calculator calculator;
+
+        [OneTimeSetUp]
+        public void Init() => calculator = new Calculator();
+
+        [Test]
+        public void DivisibleTest([Range(0, 499, 5)] int number) =>
+            Assert.That(calculator.Divisible(number), Is.EqualTo(number % 100 == 0));
+
+    }
+}
